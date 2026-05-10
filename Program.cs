b3d94144eb7b310eb4f2c40ec30110c50dfc37cd
farhan629 <farhanamfk@gmail.com>
2026-05-10 13:24:48 +0530

    enable swagger on render

--- Program.cs
+++ Program.cs
@@ -1,32 +1,24 @@
 using Microsoft.EntityFrameworkCore;
 using Employeee.Models;
-using Microsoft.Extensions.DependencyInjection;
 
 var builder = WebApplication.CreateBuilder(args);
+
 builder.WebHost.UseUrls("http://0.0.0.0:10000");
-// Add services to the container.
+
+// Add services
 builder.Services.AddControllers();
 
-
-// PostgreSQL Connection
 builder.Services.AddDbContext<EmployeeDbcContexet>(options =>
     options.UseNpgsql(
         builder.Configuration.GetConnectionString("DefaultConnection")));
 
-
-// Swagger
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseAuthorization();
 
